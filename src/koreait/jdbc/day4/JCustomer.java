2e11a36adc79782f9ff0ebebaab96d1bd153bc29
kimsohee-around <koreait.sec2020@gmail.com>
2023-06-20 13:30:34 +0900

    #5 dto
    3개 테이블 dto 정의

---
+++ src/koreait/jdbc/day4/JCustomer.java
@@ -0,0 +1,21 @@
+package koreait.jdbc.day4;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+@Builder
+@Getter
+@AllArgsConstructor
+@ToString
+//DTO : JBuy , JProduct
+public class JCustomer {	
+	private String custom_id;
+	private String name;
+	private String email;
+	private int age;
+	private Date regDate;
+
+}
